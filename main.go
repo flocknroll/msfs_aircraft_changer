3b3acded24aeb750e72cd4adea3b46128d4237eb
flocknroll <dosso.florent@gmail.com>
2023-04-23 16:59:06 +0200

    add tview

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/flocknroll/azga/go_installer/msfstools"
+	"github.com/rivo/tview"
 	"gopkg.in/ini.v1"
 )
 
@@ -11,8 +13,16 @@ func main() {
 	path, ok := msfstools.GetPackageFolderPath()
 	log.Print(path, ok)
 
-	cfg, _ := ini.Load("test_data/asobo-bushtrip-chile/Missions/Asobo/BushTrips/Chile/Chile.FLT")
+	cfg, _ := ini.Load("test_data/test.cfg")
+
+	cfg.Section("Sim.0").NewKey("# Sim", "test")
+	cfg.WriteTo(os.Stdout)
 
 	// log.Print(cfg, err)
-	log.Print(cfg.Section("Sim.0").Key("Sim"))
+	log.Print(cfg.Section("Sim.0").Key("# Sim"))
+
+	box := tview.NewBox().SetBorder(true).SetTitle("MSFS bush trips aircrafts changer")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
