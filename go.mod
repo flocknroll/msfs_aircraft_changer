1a194b6c065268f8fd2c931a5298ce788ebe89e4
flocknroll <dosso.florent@gmail.com>
2023-04-23 16:44:43 +0200

    init ini reading

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/flocknroll/msfs_aircraft_changer
 
 go 1.20
+
+require (
+	github.com/flocknroll/azga/go_installer v0.0.0-20230423115125-a3d0f6c3d377
+	gopkg.in/ini.v1 v1.67.0
+)
+
+require github.com/stretchr/testify v1.8.2 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 
 	"github.com/flocknroll/azga/go_installer/msfstools"
+	"gopkg.in/ini.v1"
 )
 
 func main() {
 	path, ok := msfstools.GetPackageFolderPath()
-
 	log.Print(path, ok)
+
+	cfg, _ := ini.Load("test_data/asobo-bushtrip-chile/Missions/Asobo/BushTrips/Chile/Chile.FLT")
+
+	// log.Print(cfg, err)
+	log.Print(cfg.Section("Sim.0").Key("Sim"))
 }
